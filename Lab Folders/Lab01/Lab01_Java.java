d71e8cee51a66ef32686c6109fbe7e296e9b8ce8
Peace Rotimi <oluwatoni.rotimi@gmail.com>
2025-01-28 21:50:15 +0000

    Added comment - Oluwatoni made edits

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -1,10 +1,12 @@
-//Lab01 Java file
+// Lab01 Java file
 // Sample Java file
 // Edit made by Oluwatoni Rotimi-Fabolude 230119666.
+// More Edits made by Oluwatoi Rotimi-Fabolude 230119666
 void testMethod1()
   {
   //First command x = y + z
   //Second commnad print(x)
+
 
 
   //Last line print('Good bye')
